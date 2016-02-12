8492be398cb042b3a9c2f570f8614cccba8b78d8
jens Leirens <jens@Maverick-PC>
2016-02-12 21:23:41 +0100

    feature 1 startsituatie aangemaakt.

---
+++ src/main/Main.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author jens
+ */
+public class Main extends Application{
+    
+     @Override
+    public void start(Stage primaryStage) {
+       // comments aanpassen en uit comment zetten eens er een gui class is
+        
+        //StackPane root = new StackPane();
+        //root.getChildren().add(btn);
+        
+        //Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Hello World!");
+        // primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
