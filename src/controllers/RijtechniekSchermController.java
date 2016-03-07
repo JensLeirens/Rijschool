84864a0d227a8443a002b6fe3aa762987cbfe7da
jens <jens@Jens>
2016-03-07 15:24:14 +0100

    bugfix en kleine aanpassingen.

--- src/controllers/RijtechniekSchermController.java
+++ src/controllers/RijtechniekSchermController.java
@@ -1,18 +1,27 @@
 
 package controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 
 
 public class RijtechniekSchermController implements Initializable {
+    
+    @FXML
+    private Node rijtechniekScherm ;
     
     @FXML
     private Button opm1 ; 
@@ -26,6 +35,14 @@ public class RijtechniekSchermController implements Initializable {
     @FXML
     private Button opm4 ; 
     
+    @FXML 
+    private Button terugknop ; 
+    @FXML
+    public void handleButtonTerugknop(ActionEvent event) throws IOException {
+        keerTerug();        
+    }
+    
+    
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         Image uitRoepTeken = new Image(getClass().getResourceAsStream("/image/Uitroepteken.jpg"));
@@ -34,8 +51,22 @@ public class RijtechniekSchermController implements Initializable {
         opm3.setGraphic(new ImageView(uitRoepTeken));
         opm4.setGraphic(new ImageView(uitRoepTeken));
         
-        Image stuur = new Image(getClass().getResourceAsStream("/image/StuurOOO.PNG"));
+        Image imgTerugknop = new Image(getClass().getResourceAsStream("/image/Terugknop.png"));
+        terugknop.setGraphic(new ImageView(imgTerugknop));
+       
         
     }
+     
     
+    public void keerTerug() throws IOException{
+        Stage currentStage = (Stage) rijtechniekScherm.getScene().getWindow();
+        currentStage.close();
+        
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/gui/HoofdScherm.fxml"));
+        Parent root1 = (Parent) fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.setTitle("Hoofdscherm");
+        stage.setScene(new Scene(root1));
+        stage.show();
+    }
 }
